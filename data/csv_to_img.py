fd694e2afd7f4bddfdc956aa199fd64dd30e4c3e
Maksim Kotenkov <maximkosmos5360@gmail.com>
2024-04-02 19:17:05 +0700

    Csv to sign-dataset

--- data/csv_to_img.py
+++ data/csv_to_img.py
@@ -14,14 +14,16 @@ image_pixels = my_data[:, 1:]
 image_size = int(np.sqrt(image_pixels.shape[1]))
 
 iter = 0
+labels = "ABCDEFGHIGKLMNOPQRSTUVWXYZ"
+
 for i in range(len(image_names)):
     image_data = image_pixels[i, :].reshape((image_size, image_size)).astype(np.uint8)
 
     image = Image.fromarray(image_data)
 
     image_name = str(int(image_names[i]))
-    image_name_iter = str(int(image_names[i]))+str(iter) + '.png'
-    if not os.path.exists(f'sign_language_dataset/mnist/{image_name}'):
-        os.makedirs(f'sign_language_dataset/mnist/{image_name}')
-    image.save(f'sign_language_dataset/mnist/{image_name}/{image_name_iter}_mnist')
+    image_name_iter = str(int(image_names[i]))+str(iter) + '_mnist.png'
+    if not os.path.exists(f'sign_language_dataset/{labels[int(image_name)]}'):
+        os.makedirs(f'sign_language_dataset/{labels[int(image_name)]}')
+    image.save(f'sign_language_dataset/{labels[int(image_name)]}/{labels[int(image_name)]}{image_name_iter}')
     iter += 1
